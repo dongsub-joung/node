14f4a3ee00438b32246215928d1cae4af0a89611
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-07-07 20:57:04 +0900

    07-07-25

--- homework-1w/main.js
+++ homework-1w/main.js
@@ -1,4 +1,4 @@
-const readline = require("readline");
+import readlineSync from 'readline-sync';
 
 class Creature {
     constructor(currentHp, attack, maxHp) {
@@ -20,47 +20,35 @@ class Creature {
 class Action {
     fight(human_obj, monster_obj){
         if(human_obj.saikoro < monster_obj.saikoro){
-            console.log(`주사위 ${monster.saikoro} vs ${human_obj.saikoro}로 몬스터가 이겼습니다.\n`);
-            human_obj.hp=- monster_obj.attack;
+            console.log(`주사위 ${monster_obj.saikoro} vs ${human_obj.saikoro}로 몬스터가 이겼습니다.\n`);
+            human_obj.currentHp=- monster_obj.attack;
+            console.log(`데미지: ${monster_obj.attack} / 남아 있는 HP: ${human_obj.currentHp}`);
+            
         }else if(human_obj.saikoro == monster_obj.saikoro){
             console.log(`주사위 ${monster.saikoro} vs ${human_obj.saikoro}로 무승무, 공격력이 높은 쪽이 이깁니다.\n`);
             if(human_obj.attack < monster_obj.attack){
-                human_obj.hp=- monster_obj.attack
+                human_obj.currentHp=- monster_obj.attack
+                console.log(`데미지: ${monster_obj.attack} / 남아 있는 HP: ${human_obj.currentHp}`);
             }else {
-                monster_obj.hp=- human_obj.attack
+                monster_obj.currentHp=- human_obj.attack
+                console.log(`남아 있는 HP: ${human_obj.currentHp}`);
             }
         }else{
-            console.log(`주사위 ${monster.saikoro} vs ${human_obj.saikoro}로 인간이 이겼습니다.\n`);
-            monster_obj.hp=- human_obj.attack;
+            console.log(`주사위 ${monster_obj.saikoro} vs ${human_obj.saikoro}로 인간이 이겼습니다.\n`);
+            monster_obj.currentHp=- human_obj.attack;
+            console.log(`남아 있는 HP: ${human_obj.currentHp}`);
         }
 
         return 0;
-    }
-
-    runAway(human_obj, monster_obj){
-        if(human_obj.getSaikoro() >= monster_obj.getSaikoro()){
-            console.log("플레이어의 주사위가 높아 도망에 성공하였습니다.\n");
-        }else{
-            console.log("플레이어의 주사위가 낮아 도망에 실패하였습니다.\n");
-        }
     }
 }
 
 
 function setAction(){
-    let input;
-
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-   readline.question('값을 입력하세요: \n', (answer) => {
-        input= answer;
-        readline.close();
-    });
-    return input;
+    const choice = readlineSync.question('Input');
+    return choice;
 }
+
 
 function lvlUp(monster_obj){
     monster_obj.currentHp=+ (monster_obj.maxHp*0.5);
@@ -73,49 +61,60 @@ function resetStatus(human_obj){
 
 
 // Init
-const HUMAN_HP= 1000;
-const HUMAN_MAXHP= 1000;
-const HUMAN_ATTACK= 50;
-let human= new Creature(HUMAN_HP, HUMAN_ATTACK, HUMAN_MAXHP);
-
-const MONSTER_HP= 500;
-const MONSTER_MAXHP= 500;
-const MONSTER_ATTACK= 200;
-let monster= new Creature(MONSTER_HP, MONSTER_ATTACK, MONSTER_MAXHP);
-
-let n= 0;
-whileRoop: while (true) {
-    let action= new Action();
+function main() {
+    const HUMAN_HP= 1000;
+    const HUMAN_MAXHP= 1000;
+    const HUMAN_ATTACK= 50;
+    let human= new Creature(HUMAN_HP, HUMAN_ATTACK, HUMAN_MAXHP);
     
-    console.log("0. 싸움\n");
-    console.log("1. 도망감\n");
-
-    let choice= 1;
-    inputRoop: while(true){
-        let choice= parseInt(setAction());
-
-        switch (choice) {
-            case 0:
-                action.fight(human, monster);
-                break inputRoop;
-            case 1:
-                action.runAway(human, monster);
-                break inputRoop;
-            default:
-                continue;
+    const MONSTER_HP= 500;
+    const MONSTER_MAXHP= 500;
+    const MONSTER_ATTACK= 200;
+    let monster= new Creature(MONSTER_HP, MONSTER_ATTACK, MONSTER_MAXHP);
+    
+    let n= 0;
+    whileRoop: while (true) {
+        let action= new Action();
+        
+        console.log("0. 싸움\n");
+        console.log("1. 도망감\n");
+    
+        let choice= 1;
+        inputRoop: while(true){
+            let choice= parseInt(setAction());
+    
+            switch (choice) {
+                case 0:
+                    action.fight(human, monster);
+                    break inputRoop;
+                case 1:
+                    {
+                        if(human.getSaikoro() >= monster.getSaikoro()){
+                            console.log("플레이어의 주사위가 높아 도망에 성공하였습니다.\n");
+                            break whileRoop;
+                        }else{
+                            console.log("플레이어의 주사위가 낮아 도망에 실패하였습니다.\n");
+                            break inputRoop;
+                        }
+                    }
+                default:
+                    continue;
+            }
+        } 
+    
+        if(human.currentHp<= 0){
+            console.log(`GAME OVER`);
+            break whileRoop;
+        }else{
+            console.log(`${++n}회 반복했습니다.`);
+            human.setNewSaikoro();
+            monster.setNewSaikoro();
+    
+            lvlUp(monster);
+            resetStatus(human);
+            continue;
         }
-    } 
-
-    if(human.currentHp<= 0){
-        console.log(`GAME OVER`);
-        break whileRoop;
-    }else{
-        console.log(`${++n}회 반복했습니다.`);
-        human.setNewSaikoro();
-        monster.setNewSaikoro();
-
-        lvlUp(monster);
-        resetStatus(human);
-        continue;
     }
 }
+
+main();
